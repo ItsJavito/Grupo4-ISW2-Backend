17b00820a480a285f5b620616b13c86cbaca2344
RicardoSilva <ricardosebastiand10s@gmail.com>
2022-09-06 10:52:39 -0500

    a

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import eventosRoutes from "./routes/eventos.routes.js"
-
+import usuariosIRoutes from "./routes/usuariosI.routes.js"
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(eventosRoutes)
+app.use(usuariosIRoutes)
 
 export default app;
