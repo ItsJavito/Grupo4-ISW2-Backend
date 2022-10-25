03e4789654d7f7aff125b365574fac53411ee594
Javier <javierenriqueos@gmail.com>
2022-10-25 17:13:44 -0500

    fix heroku

--- src/routes/eventos.routes.js
+++ src/routes/eventos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { EventoBroker } from "../brokers/eventoBroker.js";
+import { EventoBroker } from "../brokers/EventoBroker.js";
 import { Eventos } from "../models/modelos.js";
 
 const router = Router();
